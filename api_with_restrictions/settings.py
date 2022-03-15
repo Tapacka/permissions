f3c5742736d7c434af59bb14a7a00058a9002967
Tapacka <88617836+Tapacka@users.noreply.github.com>
2022-03-15 23:24:46 +0300

    12

--- api_with_restrictions/settings.py
+++ api_with_restrictions/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netology_classified_ads',
         "USER": 'postgres',
-        'PASSWORD': '42104tr',
+        'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
